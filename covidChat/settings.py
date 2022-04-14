0c03af1fd972b5085775021bf67a7cb383284614
Steven Jin <67845390+StevenJin7820@users.noreply.github.com>
2022-04-14 13:50:36 -0400

    Allowed host change

--- covidChat/settings.py
+++ covidChat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$$#9g)4t7g@opdd%3lqzq!uca&yqw!x=_px5=bmmygbk^z8v-5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://polar-crag-21006.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['polar-crag-21006.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
